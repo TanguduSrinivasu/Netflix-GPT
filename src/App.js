e49630f681bbebd9844198fa62d7456896d38f64
TanguduSrinivasu <tangudusrinivasu17@gmail.com>
2023-09-27 23:01:33 +0530

    Created the Project Structure

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className='text-3xl font-bold text-green-500'>Ome</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
